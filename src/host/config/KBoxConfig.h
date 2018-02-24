cf2d77b185735d685918e2adf2d04b200ce2a660
ronzeiller <ronnie.zeiller@gmail.com>
2018-02-24 23:41:17 +0100

    renamed to nmeaConverterConfig

--- src/host/config/KBoxConfig.h
+++ src/host/config/KBoxConfig.h
@@ -55,5 +55,5 @@ struct KBoxConfig {
   WiFiConfig wifiConfig;
   SDCardConfig sdcardConfig;
   PerformanceConfig performanceConfig;
-  SKNMEAConverterConfig nmeaConverter;
+  SKNMEAConverterConfig nmeaConverterConfig;
 };
