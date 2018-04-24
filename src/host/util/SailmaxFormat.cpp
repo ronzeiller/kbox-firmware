4f9d539641316350ea72a426f863dec549128765
ronzeiller <ronnie.zeiller@gmail.com>
2018-04-24 18:39:22 +0200

    Conversion from Sailmax Format to NMEA2000 added

--- src/host/util/SailmaxFormat.cpp
+++ src/host/util/SailmaxFormat.cpp
@@ -11,11 +11,11 @@
       * Author:   © Ronnie Zeiller, 2018
 
 */
-
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include "SailmaxFormat.h"
+#include <Seasmart.h>
 
 
 /* Some private helper functions to generate hex-serialized NMEA messages */
@@ -75,41 +75,46 @@ static uint8_t nmea_compute_checksum(const char *sentence) {
   return checksum;
 }
 
+/*
+ * Attemts to read n bytes in hexadecimal from input string to value.
+ * Returns true if successful, false otherwise.
+ */
+static bool readNHexByte(const char *s, unsigned int n, uint32_t &value) {
+  if (strlen(s) < 2*n) {
+    return -1;
+  }
+  for (unsigned int i = 0; i < 2*n; i++) {
+    if (!isxdigit(s[i])) {
+      return -1;
+    }
+  }
+
+  char sNumber[2*n + 1];
+  strncpy(sNumber, s, sizeof(sNumber));
+  sNumber[sizeof(sNumber) - 1] = 0;
+
+  value = strtol(sNumber, 0, 16);
+  return true;
+}
+
 size_t N2kToSailmax(const tN2kMsg &msg, uint32_t timestamp, char *buffer, size_t size) {
   //size_t pcdin_sentence_length = 6+1+6+1+8+1+2+1+msg.DataLen*2+1+2 + 1;
   uint32_t number = timestamp;
   int digits = 0; do { number /= 10; digits++; } while (number != 0);
+  // @millis,PGN,Source,Data*checksum
+  // @22643312,128267,23,DB28010000A0F6FF*28
   size_t pcdin_sentence_length = 1+digits+1+6+1+2+1+msg.DataLen*2+1+2 + 1;
   if (size < pcdin_sentence_length) {
     return 0;
   }
 
   char *s = buffer;
-
-  //ultoa(timestamp, s, 10 );
-
-  // @millis,PGN,Source,Data,checksum
-  //sprintf(s, "@%010lu,",timestamp );  // -> fixe 10 Stellen mit führenden Nullen!!!
-
-  //snprintf(s, sizeof (s), "@%lu,", (unsigned long) timestamp); /* Method 1 */
-  //snprintf(s, sizeof (s), "@%," PRIu32, timestamp); /* Method 2 */
   sprintf(s, "@%lu,",timestamp );
 
-  s += digits + 2;   // zur nächsten Stelle vorrücken
+  s += digits + 2;
   sprintf(s,"%06lu,",msg.PGN );
   // DebugSerial.printf(s );
   s += 7;
-
-  /*
-  strcpy(s, "@,");
-  s += 7;
-  s += appendByte(s, msg.PGN >> 16);
-  s += append2Bytes(s, msg.PGN & 0xffff);
-  *s++ = ',';
-  s += 9;
-  s += appendWord(s, timestamp);
-  *s++ = ',';
-  */
 
   s += appendByte(s, msg.Source);
   *s++ = ',';
@@ -122,4 +127,88 @@ size_t N2kToSailmax(const tN2kMsg &msg, uint32_t timestamp, char *buffer, size_t
   s += appendByte(s, nmea_compute_checksum(buffer));
   *s = 0;
   return (size_t)(s - buffer);
+}
+
+// read format: @millis,PGN,Source,Data*checksum
+// like: @22643312,128267,23,DB28010000A0F6FF*28
+bool SailmaxToN2k(const char *buffer, uint32_t &timestamp, tN2kMsg &msg) {
+
+  msg.Clear();
+  msg.Destination = 0xFF;
+
+  const char *s = buffer;
+  int i = 0;
+
+  // check if line starts with @
+  if (*s == '@') {
+    i = 1;
+
+    timestamp = 0;
+    while (s[i] != ',') {
+      timestamp = timestamp * 10 + ((int)s[i] - 48);
+      i++;
+    }
+    msg.MsgTime = timestamp;
+
+    i ++; // skip the comma
+    // the next 6 chars are the PGN (N2k page number)
+    msg.PGN = 0;
+    while (s[i] != ',') {
+      msg.PGN = msg.PGN * 10 + ((int)s[i] - 48);
+      i++;
+    }
+    i ++; // skip the comma
+    s += i;
+
+    // next comes Source
+    uint32_t source;
+    if (!readNHexByte(s, 1, source)) {
+      return false;
+    }
+    msg.Source = source;
+
+    s += 3;
+    int dataLen = 0;
+    while (s[dataLen] != 0 && s[dataLen] != '*') {
+      dataLen++;
+      //i++;
+    }
+    if (dataLen % 2 != 0) {
+      return false;
+    }
+    dataLen /= 2;
+
+    msg.DataLen = dataLen;
+    if (msg.DataLen > msg.MaxDataLen) {
+      return false;
+    }
+    //Serial.printf("DataLen: %d\n", msg.DataLen);
+
+    for (int j = 0; j < dataLen; j++) {
+      uint32_t byte;
+      if (!readNHexByte(s, 1, byte)) {
+        return false;
+      }
+      msg.Data[j] = byte;
+      s += 2;
+    }
+
+    // Skip the terminating '*' which marks beginning of checksum
+    s += 1;
+    uint32_t checksum;
+    //Serial.printf("checksum s: %s\n", s);
+    if (!readNHexByte(s, 1, checksum)) {
+      Serial.printf("readNHexByte nicht ok \n");
+      return false;
+    }
+
+    if (checksum != nmea_compute_checksum(buffer)) {
+      Serial.printf("nmea_compute_checksum nicht ok \n");
+      return false;
+    }
+
+    return true;
+  } else {
+    return false;
+  }
 }
